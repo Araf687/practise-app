fd55c5634d1ba8146a1999e8f1c1879c56ba3bf0
araf687 <arafyeasin@gmail.com>
2024-07-12 21:12:56 +0600

    ok

--- src/components/forms/Certificate/PromotionCertificateForm.tsx
+++ src/components/forms/Certificate/PromotionCertificateForm.tsx
@@ -1,29 +1,27 @@
-import React, { ReactHTMLElement, useState } from "react";
+import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { userData } from "../../../utils.tsx/fakedata/userData";
 import { useFormik } from "formik";
 import PromotionPreview from "./PromotionPreview";
 import AdvanceCertificate from "./previews/AdvanceCertificatePreview";
 
-
 interface PromotionCertificateFormProps {
-  type?:string
+  type?: string;
 }
 
-const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type}) => {
-  
-  const [initialValues, setInitialValues] = useState({
+const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({ type }) => {
+  const [initialValues] = useState({
     user_id: "",
     name: "",
     designation: "",
     applied_date: "",
     new_responsibility: "",
     approval_date: "",
-    total_advance_value:"",
-    pay_through:"",
-    installment:"",
-    installment_start_date:"",
-    approved_by:""
+    total_advance_value: "",
+    pay_through: "",
+    installment: "",
+    installment_start_date: "",
+    approved_by: ""
   });
 
   const formik = useFormik({
@@ -37,11 +35,8 @@ const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type
     console.log(values);
   };
 
-  const handleChangeInput = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    type: string
-  ) => {
-    formik.handleChange(e)
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    formik.handleChange(e);
   };
 
   return (
@@ -52,12 +47,12 @@ const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type
           <Form.Control
             as="select"
             name="user_id"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.user_id}
           >
             <option>Choose...</option>
             {userData &&
-              userData.map((item: any,index:number) => (
+              userData.map((item: any, index: number) => (
                 <option key={index} value={item.id}>{item.name}</option>
               ))}
           </Form.Control>
@@ -68,17 +63,17 @@ const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type
             type="text"
             placeholder="Enter name"
             name="name"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.name}
           />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridDropdown2">
-          <Form.Label>designation</Form.Label>
+          <Form.Label>Designation</Form.Label>
           <Form.Control
             as="select"
             name="designation"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.designation}
           >
             <option>Choose...</option>
@@ -93,20 +88,20 @@ const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type
           <Form.Label>Applied Date</Form.Label>
           <Form.Control
             type="date"
-            placeholder="Enter text"
+            placeholder="Enter date"
             name="applied_date"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.applied_date}
           />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
-          <Form.Label>New Resoinsibility</Form.Label>
+          <Form.Label>New Responsibility</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter text"
+            placeholder="Enter new responsibility"
             name="new_responsibility"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.new_responsibility}
           />
         </Form.Group>
@@ -115,80 +110,83 @@ const PromotionCertificateForm: React.FC<PromotionCertificateFormProps> = ({type
           <Form.Label>Approval Date</Form.Label>
           <Form.Control
             type="date"
-            placeholder="Enter text"
+            placeholder="Enter date"
             name="approval_date"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
+            onChange={handleChangeInput}
             value={formik.values.approval_date}
           />
         </Form.Group>
-        {type=="advance"&&<Form.Group as={Col} controlId="formGridDropdown1">
-          <Form.Label>Approved By</Form.Label>
-          <Form.Control
-            as="select"
-            name="approved_by"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
-            value={formik.values.approved_by}
-          >
-            <option>Choose...</option>
-            {userData &&
-              userData.map((item: any,index:number) => (
-                <option key={index} value={item.id}>{item.name}</option>
-              ))}
-          </Form.Control>
-        </Form.Group>}
+
+        {type === "advance" && (
+          <Form.Group as={Col} controlId="formGridDropdown1">
+            <Form.Label>Approved By</Form.Label>
+            <Form.Control
+              as="select"
+              name="approved_by"
+              onChange={handleChangeInput}
+              value={formik.values.approved_by}
+            >
+              <option>Choose...</option>
+              {userData &&
+                userData.map((item: any, index: number) => (
+                  <option key={index} value={item.id}>{item.name}</option>
+                ))}
+            </Form.Control>
+          </Form.Group>
+        )}
       </Row>
-     { type=="advance"&&<Row className="mb-3">
-        
 
-        <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
-          <Form.Label>Advance Amount</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter advance amount"
-            name="total_advance_value"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
-            value={formik.values.total_advance_value}
-          />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
-          <Form.Label>Pay through</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter pay thorugh option"
-            name="pay_through"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
-            value={formik.values.pay_through}
-          />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
-          <Form.Label>Installment</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Enter installment"
-            name="installment"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
-            value={formik.values.installment}
-          />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridDate1">
-          <Form.Label>Installment Start date</Form.Label>
-          <Form.Control
-            type="date"
-            placeholder="Enter text"
-            name="installment_start_date"
-            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleChangeInput(e,"")}
-            value={formik.values.installment_start_date}
-          />
-        </Form.Group>
-
-        
-      </Row>}
+      {type === "advance" && (
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
+            <Form.Label>Advance Amount</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter advance amount"
+              name="total_advance_value"
+              onChange={handleChangeInput}
+              value={formik.values.total_advance_value}
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
+            <Form.Label>Pay through</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter pay through option"
+              name="pay_through"
+              onChange={handleChangeInput}
+              value={formik.values.pay_through}
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridTextInput" className="mb-3">
+            <Form.Label>Installment</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter installment"
+              name="installment"
+              onChange={handleChangeInput}
+              value={formik.values.installment}
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridDate1">
+            <Form.Label>Installment Start date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Enter date"
+              name="installment_start_date"
+              onChange={handleChangeInput}
+              value={formik.values.installment_start_date}
+            />
+          </Form.Group>
+        </Row>
+      )}
 
       <Button variant="primary" type="submit">
         Submit
       </Button>
-      {type==="promotion" && <PromotionPreview data={formik.values}/>}
-      {type==="advance" && <AdvanceCertificate data={formik.values}/>}
+
+      {type === "promotion" && <PromotionPreview data={formik.values} />}
+      {type === "advance" && <AdvanceCertificate data={formik.values} />}
     </Form>
   );
 };
--- src/components/forms/Certificate/previews/AdvanceCertificatePreview.tsx
+++ src/components/forms/Certificate/previews/AdvanceCertificatePreview.tsx
@@ -7,11 +7,10 @@ interface CertificateProps {
     name: string;
     designation: string;
     total_advance_value: string;
-    totalAdvanceInWords: string;
     pay_through: string;
-    paidOn: string;
     installment: string;
     installment_start_date: string;
+    approval_date: string; // Ensure this matches the field in your form
   };
 }
 
@@ -20,12 +19,12 @@ const AdvanceCertificate: React.FC<CertificateProps> = ({ data }) => {
     name,
     designation,
     total_advance_value,
-    totalAdvanceInWords,
     pay_through,
-    paidOn,
     installment,
     installment_start_date,
+    approval_date,
   } = data;
+  const currentDate=new Date();
   return (
     <Container
       style={{
@@ -36,33 +35,39 @@ const AdvanceCertificate: React.FC<CertificateProps> = ({ data }) => {
         backgroundColor: "white",
         textAlign: "justify",
       }}
+      className="mt-5"
     >
       <Row className="mb-4">
-        <Col className="text-center">
-          <h5>Date: 14.10.2021</h5>
+        <Col>
+          <h5>Date: {currentDate.toDateString()}</h5>
         </Col>
       </Row>
-      <Row className="mb-4">
+      <Row className="mb-5">
         <Col className="text-center">
-          <u><h4>TO WHOMSOEVER IT MAY CONCERN</h4></u>
+          <u>
+            <h4>TO WHOMSOEVER IT MAY CONCERN</h4>
+          </u>
         </Col>
       </Row>
       <Row className="mb-4">
         <Col>
           <p>
-            This is to certify that Mr. {name} is working for us from 3rd of
-            March to till date. He is designated as {designation}. He applied
-            for a salary advance of Taka. {total_advance_value} (
-            {numberToWords(parseInt(total_advance_value))} Taka) on 9th August, 2020. Against his application
-            the company was satisfied to pay him the amount he desire as salary
-            advance in some conditions. Please note down the below mentioned
-            information as the main condition.
+            This is to certify that Mr. <strong>{name || "{{name}}"}</strong> is working for us from
+            3rd of March to till date. He is designated as <strong>{designation||"{{designation}}"}</strong>. He
+            applied for a salary advance of Taka. <strong>{total_advance_value||"{{total advance amount}}"} (
+                {total_advance_value?numberToWords(parseInt(total_advance_value))+" Taka":" {{In words}} "})</strong> on 9th August,
+            2020. Against his application the company was satisfied to pay him
+            the amount he desires as a salary advance under some conditions.
+            Please note down the below mentioned information as the main
+            condition.
           </p>
         </Col>
       </Row>
       <Row className="mb-4">
         <Col>
-          <u><h5>Advance Structure & Installment details</h5></u>
+          <u>
+            <h5>Advance Structure & Installment details</h5>
+          </u>
         </Col>
       </Row>
       <Row className="mb-2">
@@ -93,7 +98,7 @@ const AdvanceCertificate: React.FC<CertificateProps> = ({ data }) => {
       </Row>
       <Row className="mb-2">
         <Col>
-          <strong>Paid On:</strong> {paidOn}
+          <strong>Paid On:</strong> {approval_date}
         </Col>
       </Row>
       <Row className="mb-2">
@@ -109,12 +114,12 @@ const AdvanceCertificate: React.FC<CertificateProps> = ({ data }) => {
       <Row className="mt-4">
         <Col>
           <p>
-            Please be informed that as the installment were fixed by the equal
-            discussion of the honorable managing director and the above
-            mentioned employee so it might be change in future if any further
-            discussion takes place among the organization and the relevant
-            employee regarding advance installment. Please also note down that
-            he is committed to pay back the balance amount (if any) of his
+            Please be informed that as the installments were fixed by mutual
+            agreement between the honorable managing director and the above
+            mentioned employee, they may change in the future if further
+            discussions take place between the organization and the relevant
+            employee regarding advance installment. Also note that the employee
+            is committed to pay back any remaining balance (if any) of his
             advance before discontinuation from the organization.
           </p>
         </Col>
